f27b44ede166ac6593cd16a96019ce3075034d65
MoonJang <roby8502@naver.com>
2022-07-09 17:15:34 +0900

    ADD: test common class 추가

---
+++ backend/src/test/java/kr/mj/polling/integration/common/BaseIntegrationTest.java
@@ -0,0 +1,25 @@
+package kr.mj.polling.integration.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles(value = "test")
+public class BaseIntegrationTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+}
+
---
+++ backend/src/test/java/kr/mj/polling/unit/user/factory/UserFactory.java
@@ -0,0 +1,46 @@
+package kr.mj.polling.unit.user.factory;
+
+import kr.mj.polling.user.entity.User;
+import kr.mj.polling.user.type.UserRoleType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserFactory {
+
+    public static final Long TEST_ID = 1L;
+    public static final String TEST_UNIQUE_ID = "testUserId";
+    public static final String TEST_NICK_NAME = "홍길동";
+    public static final String TEST_PASSWORD = "test1234*";
+    public static final String TEST_REG_NO = "860824-1655068";
+    public static final UserRoleType TEST_USER_ROLE_TYPE = UserRoleType.ROLE_USER;
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public static User create() {
+        return User.builder()
+                .uniqueId(TEST_UNIQUE_ID)
+                .nickName(TEST_NICK_NAME)
+                .password(passwordEncoder.encode(TEST_PASSWORD))
+                .userRole(TEST_USER_ROLE_TYPE)
+                .build();
+    }
+
+    public static User create(String uniqueId) {
+        return User.builder()
+                .uniqueId(uniqueId)
+                .nickName(TEST_NICK_NAME)
+                .password(passwordEncoder.encode(TEST_PASSWORD))
+                .userRole(TEST_USER_ROLE_TYPE)
+                .build();
+    }
+
+    public static User createWithId() {
+        return User.builder()
+                .id(TEST_ID)
+                .uniqueId(TEST_UNIQUE_ID)
+                .nickName(TEST_NICK_NAME)
+                .password(passwordEncoder.encode(TEST_PASSWORD))
+                .userRole(TEST_USER_ROLE_TYPE)
+                .build();
+    }
+
+}
