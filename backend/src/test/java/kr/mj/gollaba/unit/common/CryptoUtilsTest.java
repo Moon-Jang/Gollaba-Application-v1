dc2458a5f3d9705379f2d383deb57aecbb6443fc
MoonJang <roby8502@naver.com>
2022-08-22 21:47:57 +0900

    ADD: 암호 유틸 테스트 추가

---
+++ backend/src/test/java/kr/mj/gollaba/unit/common/CryptoUtilsTest.java
@@ -0,0 +1,48 @@
+package kr.mj.gollaba.unit.common;
+
+
+import kr.mj.gollaba.common.util.CryptUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = { "spring.config.location=classpath:private/application-test.yaml", "security.aes.encrypt-key=asd" })
+@ActiveProfiles("test")
+public class CryptoUtilsTest {
+
+
+	private final String encryptKey;
+
+	private final CryptUtils cryptUtils;
+
+	public CryptoUtilsTest(@Value("${security.aes.encrypt-key}") String encryptKey) {
+		this.encryptKey = encryptKey;
+		this.cryptUtils = new CryptUtils(encryptKey);
+	}
+
+	@DisplayName("문자열 암호화 테스트")
+	@Test
+	void cryptoTest() {
+	    //given
+		String str = "hello";
+
+	    //when
+	    String encryptedStr = cryptUtils.encrypt(str);
+		System.out.println(encryptedStr);
+
+		//then
+		assertThat(cryptUtils.decrypt(encryptedStr)).isEqualTo(str);
+	}
+
+}
