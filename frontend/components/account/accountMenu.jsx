7f969326c428a4e7ded3747c7afda8cd986baa6d
Kim-Marvin <56723684+Kim-Marvin@users.noreply.github.com>
2022-08-23 23:23:07 +0900

    km account page commit 220823

---
+++ frontend/components/account/accountMenu.jsx
@@ -0,0 +1,10 @@
+export default function AccountMenu() {
+    return (
+        <>
+            <div>내 투표</div>
+            <div>즐겨찾기</div>
+            <div>비밀번호 변경</div>
+            <div>로그아웃</div>
+        </>
+    )
+}
---
+++ frontend/components/account/profile.jsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+    return (
+        <>
+            <div>배경이미지</div>
+            <div>프로필이미지 (수정 가능)</div>
+            <div>닉네임 공간 (수정 가능)</div>
+        </>
+    )
+}
