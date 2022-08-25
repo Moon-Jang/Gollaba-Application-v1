d825d1a5390df91783e38504d0c737d6b0c56956
Kim-Marvin <56723684+Kim-Marvin@users.noreply.github.com>
2022-08-25 22:27:18 +0900

    220825 km

---
+++ frontend/components/account/passwordChange.jsx
@@ -0,0 +1,22 @@
+import { Button } from '@mui/material'
+import { Link } from 'react-router-dom'
+
+export default function PasswordChange() {
+    return (
+        <>
+            <div>비밀번호 변경 기능 컴포넌트</div>
+            <Link href='/*'>
+                <Button
+                    color='primary'
+                    type='submit'
+                    variant='outlined'
+                    fullWidth
+                    style={{ verticalAlign: 'middle', color: '#000000' }}
+                    sx={{ mt: 4.5, mb: 2, borderRadius: 12.5, boxShadow: 4 }}
+                >
+                    비밀번호 변경
+                </Button>
+            </Link>
+        </>
+    )
+}
