b3b9167d0ac837854fea0051c13f28481037821a
Kim-Marvin <56723684+Kim-Marvin@users.noreply.github.com>
2022-08-26 22:46:58 +0900

    220826 km commit

--- frontend/components/account/accountMenu.jsx
+++ frontend/components/account/accountMenu.jsx
@@ -1,4 +1,5 @@
 import Logout from './logout'
+import PasswordChange from './passwordChange'
 
 export default function AccountMenu() {
     return (
@@ -6,6 +7,7 @@ export default function AccountMenu() {
             <div>내 투표</div>
             <div>즐겨찾기</div>
             <div>비밀번호 변경</div>
+            <PasswordChange />
             <div>로그아웃</div>
             <Logout />
         </>
