50e0b6fead00bb21208ba4dc4eafa007d275065c
MoonJang <roby8502@naver.com>
2022-08-27 14:37:48 +0900

    UPDATE: voting page Path Parameter 추가

--- frontend/pages/voting/[pollId].jsx
+++ frontend/pages/voting/[pollId].jsx
@@ -1,31 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import ProTip from "../src/ProTip";
-import Link from "../src/Link";
-import Checkbox from "@mui/material/Checkbox";
-import Grid from "@mui/material/Grid";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
 
-import ButtonAppBar from "../components/buttonAppBar";
-import FooterNav from "../components/footerNav";
+import ButtonAppBar from "../../components/buttonAppBar";
+import FooterNav from "../../components/footerNav";
 import axios from "axios";
-import Description from "../components/voting/description";
-import MapOption from "../components/voting/mapOption";
-import CreateBtn from "../components/voting/createBtn";
+import Description from "../../components/voting/description";
+import MapOption from "../../components/voting/mapOption";
+import CreateBtn from "../../components/voting/createBtn";
 import { useRouter } from "next/router";
 
 const theme = createTheme({
@@ -43,9 +28,8 @@ const theme = createTheme({
 export default function Voting() {
   const router = useRouter();
   let response;
-  const params = new URLSearchParams(window.location.search);
-  let pollId = params.get("pollId");
-
+  const {pollId} = router.query;
+  console.log(pollId)
   const [selected, setSelected] = useState([]);
   const [polls, setPolls] = useState([]);
 
@@ -65,8 +49,10 @@ export default function Voting() {
   };
 
   useEffect(() => {
-    getData();
-  }, []);
+    if (pollId) {
+      getData();
+    }
+  }, [pollId]);
 
   const [voted, setVoted] = useState([]);
 
