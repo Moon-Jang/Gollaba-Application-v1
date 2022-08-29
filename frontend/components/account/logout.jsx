56eede4d566a8f93f7aa52fc2f9ae51e6dc2a465
Kim-Marvin <56723684+Kim-Marvin@users.noreply.github.com>
2022-08-29 23:22:41 +0900

    22829 km commit

--- frontend/components/account/logout.jsx
+++ frontend/components/account/logout.jsx
@@ -1,24 +1,23 @@
 import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
+import Link from 'next/link'
 
 export default function Logout() {
     // useRouter , NextLink
     return (
         <>
             <div>로그아웃 기능 컴포넌트</div>
-            {/* <Link href='/index'> */}
-
-            <Button
-                color='primary'
-                type='submit'
-                variant='outlined'
-                fullWidth
-                style={{ verticalAlign: 'middle', color: '#000000' }}
-                sx={{ mt: 4.5, mb: 2, borderRadius: 12.5, boxShadow: 4 }}
-            >
-                로그아웃
-            </Button>
-            {/* </Link> */}
+            <Link href='/'>
+                <Button
+                    color='primary'
+                    type='submit'
+                    variant='outlined'
+                    fullWidth
+                    style={{ verticalAlign: 'middle', color: '#000000' }}
+                    sx={{ mt: 4.5, mb: 2, borderRadius: 12.5, boxShadow: 4 }}
+                >
+                    로그아웃
+                </Button>
+            </Link>
         </>
     )
 }
