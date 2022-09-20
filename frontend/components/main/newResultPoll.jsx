8f6febcef44a7373f74a477f2c6a3c230ab4003e
Johnsnim <qw506799@naver.com>
2022-09-20 22:23:04 +0900

    UPDATE: main / index 서로 변경

---
+++ frontend/components/main/newResultPoll.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { width } from "@mui/system";
+import Options from "./options";
+
+export default function NewResultPoll(props) {
+  const OptionsMap = () => {
+    const data = props.data.options;
+    return data.map((el) => <Options data={el} />);
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "150px",
+        width: "300px",
+        backgroundColor: "rgb(230,230,230)",
+        mr: 2,
+        borderRadius: 3,
+        flexShrink: 0,
+        flexDirection: "column",
+        //alignItems: "flex-end",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          height: "70px",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {props.data.title}
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          height: "80px",
+          backgroundColor: "rgb(180,180,180)",
+          borderRadius: 3,
+          borderTopRightRadius: 0,
+          borderTopLeftRadius: 0,
+          borderTop: 1,
+        }}
+      >
+        {OptionsMap()}
+      </Box>
+    </Box>
+  );
+}
