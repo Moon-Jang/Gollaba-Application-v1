e732788479f8d8f4058f52f5d0064cf03effcf73
Kim-Marvin <kgmng920@gmail.com>
2022-10-11 23:33:55 +0900

    UPDATE : 회원가입 API 완료

--- frontend/components/signupComponent.jsx
+++ frontend/components/signupComponent.jsx
@@ -1,25 +1,36 @@
-import { Avatar, Box, IconButton, TextField, Button, CssBaseline } from "@mui/material"
+import { Avatar, Box, IconButton, TextField, Button, CssBaseline, ThemeProvider } from "@mui/material"
 import { useRef, useState, useEffect } from "react"
 import { useCookies } from "react-cookie"
 import jwt from "jsonwebtoken"
 import ApiGateway from "../apis/ApiGateway"
 import CommonValidator from "./../utils/CommonValidator"
+import theme from "./../src/theme"
+import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
+import { useRouter } from "next/router"
 
 export default function SignupComponent() {
+    const router = useRouter()
+
     //Profile, Background Image 변경
 
     const PROFILE_BASIC = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     const [profileImage, setProfileImage] = useState(PROFILE_BASIC)
     const photoInput = useRef(null)
+    const imagePayload = useRef(null)
+
     const handleClick = () => {
         photoInput.current.click()
     }
 
     const changeProfile = async e => {
+        const profileImageSelected = e.target.files[0]
         if (e.target.files[0]) {
-            setProfileImage(e.target.files[0])
+            // setProfileImage(e.target.files[0])
+            imagePayload.current = e.target.files[0]
         } else {
             setProfileImage(PROFILE_BASIC)
+
             return
         }
         const reader = new FileReader()
@@ -90,14 +101,16 @@ export default function SignupComponent() {
     }
 
     const signupHandler = async e => {
+        e.preventDefault()
+        console.log("profileImage : ", profileImage)
+        // console.log("profileImageSelected : ", changeProfile[profileImageSelected])
+        // console.log()
         try {
-            const photoToAdd = e.target.files[0]
-            console.log("photoToAdd", photoToAdd)
             const formData = new FormData()
             formData.append("id", id)
             formData.append("nickName", nickName)
             formData.append("password", password)
-            formData.append("profileImage", photoToAdd)
+            formData.append("profileImage", imagePayload.current)
             for (const keyValue of formData) console.log("keyValue : ", keyValue)
             const signup = await ApiGateway.signupForm(formData)
             console.log("signup", signup)
@@ -105,114 +118,123 @@ export default function SignupComponent() {
         } catch (e) {
             console.log(e)
         }
+        router.push("/login")
     }
 
     return (
         <>
-            <Box
-                component="form"
-                onSubmit={signupHandler}
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <div style={{ marginTop: 80 }}>
-                    <h1>회원 가입</h1>
-                </div>
-                <div>
-                    <Avatar
-                        src={profileImage}
-                        id="profileImage"
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+
+                    <Box
+                        component="form"
+                        onSubmit={signupHandler}
                         sx={{
-                            width: 215,
-                            height: 215,
-                            objectFit: "cover",
-                            // position: "absolute",
-                            marginTop: 5,
-                            marginBottom: 2,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            justifyContent: "center",
                         }}
-                        onClick={handleClick}
-                    />
-                    <input
-                        type="file"
-                        id="profileImageInput"
-                        style={{ display: "none" }}
-                        accept="image/jpg image/jpeg image/png"
-                        onChange={e => changeProfile(e)}
-                        ref={photoInput}
-                    />
-                </div>
-
-                <div>
-                    <span>프로필 사진을 눌러 변경해 주세요</span>
-                </div>
-
-                <div style={{ marginTop: 20 }}>
-                    <TextField
-                        required
-                        fullWidth
-                        margin="dense"
-                        variant="standard"
-                        id="id"
-                        name="id"
-                        label="아이디"
-                        helperText={helperTextId}
-                        error={isErrorId ? true : false}
-                        onChange={onChangeIdHandler}
-                    />
-                    <TextField
-                        required
-                        fullWidth
-                        margin="dense"
-                        variant="standard"
-                        id="nickName"
-                        name="nickName"
-                        label="닉네임"
-                        helperText={helperTextNickName}
-                        error={isErrorNickName ? true : false}
-                        onChange={onChangeNickNameHandler}
-                    />
-                    <TextField
-                        required
-                        fullWidth
-                        margin="dense"
-                        variant="standard"
-                        id="password"
-                        name="password"
-                        type="password"
-                        label="비밀번호"
-                        helperText={helperTextPassword}
-                        error={isErrorPassword ? true : false}
-                        onChange={onChangePasswordHandler}
-                    />
-                    <TextField
-                        required
-                        fullWidth
-                        margin="dense"
-                        variant="standard"
-                        id="passwordCheck"
-                        name="passwordCheck"
-                        type="password"
-                        label="비밀번호 확인"
-                        helperText={helperTextPasswordCheck}
-                        error={isErrorPasswordCheck ? true : false}
-                        onChange={onChangePasswordCheckHandler}
-                    />
-                    <Button
-                        fullWidth
-                        type="submit"
-                        color="primary"
-                        variant="outlined"
-                        style={{ verticalAlign: "middle", color: "#000000" }}
-                        sx={{ mt: 4.5, mb: 2, borderRadius: 12.5, boxShadow: 4 }}
                     >
-                        회원가입
-                    </Button>
-                </div>
-            </Box>
+                        <div style={{ marginTop: 80 }}>
+                            <Typography component="h1" varient="h1" sx={{ fontWeight: "bold", fontSize: 38 }}>
+                                회원 가입
+                            </Typography>
+                        </div>
+                        <div>
+                            <Avatar
+                                src={profileImage}
+                                id="profileImage"
+                                sx={{
+                                    width: 215,
+                                    height: 215,
+                                    objectFit: "cover",
+                                    // position: "absolute",
+                                    marginTop: 5,
+                                    marginBottom: 2,
+                                }}
+                                onClick={handleClick}
+                            />
+                            <input
+                                type="file"
+                                id="profileImageInput"
+                                style={{ display: "none" }}
+                                accept="image/jpg image/jpeg image/png"
+                                onChange={e => changeProfile(e)}
+                                ref={photoInput}
+                            />
+                        </div>
+
+                        <div>
+                            <span>프로필 사진을 변경해 주세요</span>
+                        </div>
+
+                        <div style={{ marginTop: 20 }}>
+                            <TextField
+                                required
+                                fullWidth
+                                margin="dense"
+                                variant="standard"
+                                id="id"
+                                name="id"
+                                label="아이디"
+                                helperText={helperTextId}
+                                error={isErrorId ? true : false}
+                                onChange={onChangeIdHandler}
+                            />
+                            <TextField
+                                required
+                                fullWidth
+                                margin="dense"
+                                variant="standard"
+                                id="nickName"
+                                name="nickName"
+                                label="닉네임"
+                                helperText={helperTextNickName}
+                                error={isErrorNickName ? true : false}
+                                onChange={onChangeNickNameHandler}
+                            />
+                            <TextField
+                                required
+                                fullWidth
+                                margin="dense"
+                                variant="standard"
+                                id="password"
+                                name="password"
+                                type="password"
+                                label="비밀번호"
+                                helperText={helperTextPassword}
+                                error={isErrorPassword ? true : false}
+                                onChange={onChangePasswordHandler}
+                            />
+                            <TextField
+                                required
+                                fullWidth
+                                margin="dense"
+                                variant="standard"
+                                id="passwordCheck"
+                                name="passwordCheck"
+                                type="password"
+                                label="비밀번호 확인"
+                                helperText={helperTextPasswordCheck}
+                                error={isErrorPasswordCheck ? true : false}
+                                onChange={onChangePasswordCheckHandler}
+                            />
+                            <Button
+                                fullWidth
+                                type="submit"
+                                color="primary"
+                                variant="outlined"
+                                style={{ verticalAlign: "middle", color: "#000000" }}
+                                sx={{ mt: 4.5, mb: 2, borderRadius: 12.5, boxShadow: 4 }}
+                            >
+                                회원가입
+                            </Button>
+                        </div>
+                    </Box>
+                </Container>
+            </ThemeProvider>
         </>
     )
 }
