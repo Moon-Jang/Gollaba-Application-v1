00a022833fa7cb901b039e961d00053dd597154b
Moon-Jang <roby8502@naver.com>
2022-10-23 14:02:19 +0900

    ADD: 즐겨찾기 조회 기능 추가

--- backend/src/main/java/kr/mj/gollaba/favorites/controller/FavoritesController.java
+++ backend/src/main/java/kr/mj/gollaba/favorites/controller/FavoritesController.java
@@ -11,11 +11,13 @@ import kr.mj.gollaba.common.Const;
 import kr.mj.gollaba.common.ErrorAPIResponse;
 import kr.mj.gollaba.favorites.dto.CreateFavoritesRequest;
 import kr.mj.gollaba.favorites.dto.CreateFavoritesResponse;
+import kr.mj.gollaba.favorites.dto.FindAllFavoritesResponse;
 import kr.mj.gollaba.favorites.service.FavoritesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +26,27 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 @RequestMapping(Const.ROOT_URL)
 @Api(tags = "Favorites")
+@PreAuthorize("hasRole('ROLE_USER')")
 @RequiredArgsConstructor
 public class FavoritesController {
 
     private final FavoritesService favoritesService;
 
-    @ApiOperation(value = "즐겨찾기")
+
+    @ApiOperation(value = "즐겨찾기 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = FindAllFavoritesResponse.class))),
+            @ApiResponse(responseCode = "400", description = "에러", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = ErrorAPIResponse.class)))})
+    @GetMapping(path = "/favorites")
+    public ResponseEntity<FindAllFavoritesResponse> findAll(@ApiIgnore @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(favoritesService.findAll(principalDetails.getUser()));
+    }
+
+    @ApiOperation(value = "즐겨찾기 생성")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                     schema = @Schema(implementation = CreateFavoritesResponse.class))),
